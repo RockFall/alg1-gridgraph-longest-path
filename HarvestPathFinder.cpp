41c99030f6cc23caeadf33fbdd33ec174c7ed4bd
RockFall <caiohdrocha@gmail.com>
2022-02-10 16:10:33 -0300

    Improvements

--- HarvestPathFinder.cpp
+++ HarvestPathFinder.cpp
@@ -1,6 +1,6 @@
 #include "HarvestPathFinder.hpp"
 
-#include "iostream"
+#include <iostream>
 #include <initializer_list>
 #include <algorithm>
 
@@ -8,24 +8,32 @@ HarvestPathFinder::Result HarvestPathFinder::FindPath()
 {
   Result result = Result();
   int rows, apple_tree_count;
+
+  // First line
   std::cin >> rows >> apple_tree_count;
 
   // * This algorithm runs while it reads the data *
   //
   // For each apple_tree it evaluates the previous 3 or 2
-  // values that it is connected to
+  // values that it is connected to:
+  //
   // i.e.:  OLD OLD old old
-  //        NEW new new new  (upper-case connected to uppercase)
+  //         | /
+  //        NEW new new new
+  //
   // i.e.2: OLD OLD OLD old
+  //           \ | /
   //        new NEW new new
-  // and selects the bigger one, addinG it to it's count
-  // i.e: old 2 4 2 2      2 4 2 2
-  //      new 1 2 3 1  ->  5 6 7 3
+  //
+  // and selects the bigger one, adding it to it's count
+  // i.e:         2   4   2   2  |   2   4   2   2
+  //      newLine 1   2   3   1  -> 1+4 2+4 3+4 1+2
 
   std::vector<int> empty;
   std::vector<std::vector<int>> apples_matrix(rows, empty);
 
-  // First row without evaluation
+
+  // First row: without evaluation
   for (int i = 0; i < apple_tree_count; ++i)
   {
     int curr;
@@ -73,14 +81,66 @@ HarvestPathFinder::Result HarvestPathFinder::FindPath()
     }
   }
 
-  for (int i = 0; i < rows; ++i)
-  {
-    for (int j = 0; j < apple_tree_count; ++j)
-    {
-      std::cout << apples_matrix[i][j] << " ";
-    }
-    std::cout << std::endl;
-  }
+  TracebackResult(result, apples_matrix);
+  PrintResult(result);
 
   return result;
+}
+
+void HarvestPathFinder::TracebackResult(Result &result, const std::vector<std::vector<int>> &apples_matrix) {
+  // Max of the last row is the solution's total apples
+  auto max_apples_it = std::max_element(apples_matrix.back().begin(), apples_matrix.back().end());
+  result.total_apples = *max_apples_it;
+
+  int row = apples_matrix.size() - 1;
+  int column = std::distance(apples_matrix.back().begin(), max_apples_it);
+
+  result.path.push(column);
+
+  // Traceback the path
+  while(row != 0) {
+    // We have three or two possible ways to go:
+    // Diagonal left, up or diagonal right
+
+    // We first select the straight up apple tree
+    int max_apples = apples_matrix[row-1][column];
+    int new_column = column;
+
+    // The we test diagonals
+    if (column != 0) {
+      // Left diagonal
+      if (max_apples < apples_matrix[row - 1][column - 1]) {
+        max_apples = apples_matrix[row - 1][column - 1];
+        new_column = column - 1;
+      }
+    }
+    if (column != int(apples_matrix[0].size())-1) {
+      // Right diagonal
+      if (max_apples < apples_matrix[row - 1][column + 1]) {
+        max_apples = apples_matrix[row - 1][column + 1];
+        new_column = column + 1;
+      }
+    }
+
+    result.path.push(new_column);
+
+    column = new_column;
+    row -= 1;
+  }
+}
+
+void HarvestPathFinder::PrintResult(const Result& result) {
+  // Print apple count
+
+
+  std::stack<long long int> out = result.path;
+  while (out.size() > 1) {
+    std::cout << out.top() << ' ';
+    out.pop();
+  }
+  // Print last element without space ' '
+  if (!out.empty())
+    std::cout << out.top() << std::endl;
+
+  std::cout << result.total_apples << std::endl;
 }
--- HarvestPathFinder.hpp
+++ HarvestPathFinder.hpp
@@ -1,20 +1,24 @@
 #pragma once
 
+#include <stack>
 #include <vector>
 
-
-
+// From standart input, finds the best solution to the
+// Rows of Apple Tree's Problem
 class HarvestPathFinder
 {
 public:
+  // Holds the result found
   struct Result {
-    int total_apples;
-    std::vector<int> path;
+    long long int total_apples;
+    std::stack<long long int> path;
   };
 
+  // Find's the most optimal path of the Problem
   static Result FindPath();
-
+  // Prints the total count of apples and the indexing of thr path recorded
+  static void PrintResult(const Result &result);
 private:
-
-
+  // From bottom to top, records the path in the Result struct
+  static void TracebackResult(Result &result, const std::vector<std::vector<int>> &apples_matrix);
 };
